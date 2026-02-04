005736db842a7e40787181d7761f2840c81276eb
ratzratul <f.rabbi.ratz@gmail.com>
2026-02-04 18:22:33 +0600

    Update script.js

--- script.js
+++ script.js
@@ -712,13 +712,13 @@ function generatePrintView()  {
             <p>মাস শেষের মিটার রিডিং: ${enToBnNumber(curr)}</p>
             <p>মাস শুরুর মিটার রিডিং: ${enToBnNumber(prev)}</p>
             <p>ব্যবহৃত ইউনিট: ${enToBnNumber(units.toFixed(0))}</p>
-            <p><strong>বিদ্যুৎ বিল (${enToBnNumber(rate)}৳ হারে): ${enToBnNumber(eBill.toFixed(0))}/-</strong></p>
+            <p><strong>বিদ্যুৎ বিল </strong>(${enToBnNumber(rate)}৳ হারে): <strong>${enToBnNumber(eBill.toFixed(0))}/-</strong></p>
             <p>মাসিক ভাড়া: ${enToBnNumber(rent)}/-</p>
             <p>${id === "6B" ? "গ্যাস বিল ও সিঁড়ি ঝাড়ু" : "সিঁড়ি ঝাড়ু"}: ${enToBnNumber(serv)}/-</p>
             <p><strong>${targetMonthName} মাসের বকেয়া: ${enToBnNumber(dues.toFixed(0))}৳</strong></p>
             ${advNote}
             <p class="total-row"><strong>এই মাসে মোট পাওনা: ${enToBnNumber(total.toFixed(0))}৳</strong></p>
-            <div style="margin-top:10px; font-size:12px; border-top:1px solid #000; padding-top:6px; text-align: center; line-height: 1.3;">
+            <div style="margin-top:10px; font-size:12px; font-style: italic; border-top:1px solid #000; padding-top:6px; text-align: center; line-height: 1.3;">
             <strong>প্রতি মাসের ৫ তারিখের মধ্যে কারেন্ট বিলের টাকা বিকাশ করতে হবে। বিকাশ নাম্বার: 01944529442 রেফারেন্স (Ref): ${id}</strong>
             </div>
             </div>`;
@@ -729,4 +729,5 @@ function generatePrintView()  {
   });
   window.print();
 }
+
 
