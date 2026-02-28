d6bb736bf6106735da3a24fc727812f0bd712b9d
ratzratul <f.rabbi.ratz@gmail.com>
2026-03-01 05:54:44 +0600

    Update script.js

--- script.js
+++ script.js
@@ -709,7 +709,7 @@ function generatePrintView()  {
       const dues = (lTot - lPad);
       const total = eBill + rent + serv + dues;
       // অ্যাডভান্স নোট লজিক
-      const advNote = (typeof adjustedAdvances !== 'undefined' && adjustedAdvances[id]) ? `<em style="color: #006666; font-size: 12px; font-style: italic; margin: 2px 0; text-align: center;">* অ্যাডভান্স ৳${enToBnNumber(adjustedAdvances[id])} বাদ দেয়া হয়েছে।</em>` : "";
+      const advNote = (typeof adjustedAdvances !== 'undefined' && adjustedAdvances[id]) ? `<div style="color: #006666; font-size: 12px; font-style: italic; margin: 2px 0; text-align: center;">* অ্যাডভান্স ৳${enToBnNumber(adjustedAdvances[id])} বাদ দেয়া হয়েছে।</div>` : "";
       const shortFormattedMonth = formattedMonth.replace(" - ২০", " - '");
       const bsrgo = '<span style="display:inline-flex; width:0.5em; overflow:hidden; justify-content:flex-end; vertical-align:baseline;">বঃ</span>';
       html += `
@@ -735,6 +735,7 @@ function generatePrintView()  {
   });
   window.print();
 }
+
 
 
 
